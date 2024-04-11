eaf1d4cc0cb2dc1fa7d686ae898340b184c505e8
JoelDiazPA <joeldiazportalesandreu@gmail.com>
2024-04-11 17:34:42 +0200

    Boton flotante hecho (no funcional)

--- src/anonymy/pages/AnonymyPage.jsx
+++ src/anonymy/pages/AnonymyPage.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import { AnonymyLayout } from '../layout/AnonymyLayout'
+import { IconButton } from '@mui/material'
+import { AddRounded } from '@mui/icons-material'
 
 export const AnonymyPage = () => {
   return (
     <AnonymyLayout>
       Anonymy hecho por Karolyn y Joel
+
+      <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'secondary.main',
+          ':hover': { backgroundColor: 'secondary.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddRounded sx={{ fontSize: 30 }} />
+      </IconButton>
+      
+
     </AnonymyLayout>
+    
   )
 }
